251ba201064404cc118fb056efc883ac2fcb0762
Janine Giducos <janinegiducos65@gmail.com>
2024-11-13 15:09:26 +0800

    Recreation

--- src/main/java/com/g3appdev/noteably/Repository/TimerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.g3appdev.noteably.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.g3appdev.noteably.entity.TimerEntity;
-
-public interface TimerRepository extends JpaRepository<TimerEntity, Integer> {
-}
