d7f57bc91bfdc3feeb2246f057424c1228385502
MuttiaSelgas <selgasmuttia@gmail.com>
2024-11-17 15:35:05 +0800

    Muttia Selgas - Backend connect

--- backend/noteably/src/main/java/com/g3appdev/noteably/noteably/Entity/FolderEntity.java
+++ backend/noteably/src/main/java/com/g3appdev/noteably/noteably/Entity/FolderEntity.java
@@ -1,12 +1,15 @@
 package com.g3appdev.noteably.noteably.Entity;
 
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class FolderEntity {
@@ -15,10 +18,14 @@ public class FolderEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int folderId;
 
-    @Column(nullable = false)
+    @Column(name = "title")
     private String title;
 
     private Integer dashboardId;
+
+    // One folder can have many notes
+    @OneToMany(mappedBy = "folderId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<NoteEntity> notes;
 
     // Default constructor
     public FolderEntity() {}
@@ -46,6 +53,14 @@ public class FolderEntity {
 
     public void setDashboardId(Integer dashboardId) {
         this.dashboardId = dashboardId;
+    }
+
+    public List<NoteEntity> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(List<NoteEntity> notes) {
+        this.notes = notes;
     }
 
     @Override
