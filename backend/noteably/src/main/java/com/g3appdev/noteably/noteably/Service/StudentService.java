2abd2385ddde7a45ed073e8c944066cc445a1ad7
Sophieanneka10 <sophieanneka.seismundo@cit.edu>
2024-11-17 13:49:48 +0800

    updated

--- backend/noteably/src/main/java/com/g3appdev/noteably/noteably/Service/StudentService.java
+++ backend/noteably/src/main/java/com/g3appdev/noteably/noteably/Service/StudentService.java
@@ -1,7 +1,7 @@
 package com.g3appdev.noteably.noteably.Service;
 
-import com.g3appdev.noteably.noteably.Entity.ScheduleEntity;
-import com.g3appdev.noteably.noteably.Repository.ScheduleRepository;
+import noteably.noteably.Entity.StudentEntity;
+import noteably.noteably.Repository.StudentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.crypto.password.PasswordEncoder;
