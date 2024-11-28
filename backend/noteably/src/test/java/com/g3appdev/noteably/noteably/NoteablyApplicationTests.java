5cff6bec5d8a3463d6b861a46042d4163207b1f4
giducos_janine <janinegiducos65@gmail.com>
2024-11-28 18:09:27 +0800

    NEW REPO

---
+++ backend/noteably/src/test/java/com/g3appdev/noteably/noteably/NoteablyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g3appdev.noteably.noteably;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteablyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
