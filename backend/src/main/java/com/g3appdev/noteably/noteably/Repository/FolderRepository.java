b77a089b1e057704e30081ea6405e60ac2be1986
giducos_janine <112496509+X123X456X@users.noreply.github.com>
2024-11-28 15:53:11 +0800

    Delete backend directory

--- backend/src/main/java/com/g3appdev/noteably/noteably/Repository/FolderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.g3appdev.noteably.noteably.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.g3appdev.noteably.noteably.Entity.FolderEntity;
-
-public interface FolderRepository extends JpaRepository<FolderEntity, Integer> {
-    // You can add custom query methods here if needed
-}
